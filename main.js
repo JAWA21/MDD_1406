afb2f81bd8b0aaca5b3029e05e4f9e3e5c8bbe09
Jeanna <jawa@fullsail.edu>
2014-06-29 13:12:34 -0400

    two click navigation - back to top

---
+++ main.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  // back to top
+  // window height + offset
+  if ( ($(window).height() + 100) < $(document).height() ) {
+      $('#top-link-block').removeClass('hidden').affix({
+          // how far to scroll down before link "slides" into view
+          offset: {top:100}
+      });
+  }
+
+  // smooth scrolling from nav
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
