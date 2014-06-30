4d86c71ccfc23244be42c1ff7b08c4b57a2f4fbc
Jeanna <jawa@fullsail.edu>
2014-06-29 23:42:04 -0400

    uploading php file for contact

---
+++ mail.php
@@ -0,0 +1,25 @@
+<?php
+$to = 'scatzz88@gmail.com';
+
+$subject = 'Website contact';
+
+$headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+// $headers .= "CC: susan@example.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$message = '<html><body>';
+$message .= '<img src="http://css-tricks.com/examples/WebsiteChangeRequestForm/images/wcrf-header.png" alt="Website Change Request" />';
+$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['inputName']) . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['inputEmail']) . "</td></tr>";
+
+$curText = htmlentities($_POST['inputComment']);
+if (($curText) != '') {
+    $message .= "<tr><td><strong>CURRENT Content:</strong> </td><td>" . $curText . "</td></tr>";
+}
+$message .= "</table>";
+$message .= "</body></html>";
+
+?>
