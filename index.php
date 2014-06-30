a6acc38fd7bfaf32557cbf4ff592a015e716c922
Jeanna <jawa@fullsail.edu>
2014-06-30 16:13:12 -0400

    playing around with email and php

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Jeanna Anderson's Port</title>
+    <meta name="description" content="Portfolio of Jeanna Anderson">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CDN Styles-->
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="box">
+        <div class="col-lg-12 text-center header">
+          <h1><small class="welcome">Welcome to</small><hr class="tagline-divider"><span class="brand-name"><b>Jeanna Anderson's portfolio</b></span><hr class="tagline-divider"></h1>
+          <ul class="nav nav-pills nav-justified">
+            <li><a href="#about" class="active">About</a></li>
+            <li><a href="#port">Portfolio</a></li>
+            <li><a href="#resume">Resume</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-12 hero">
+        <img class="img-responsive" src="https://lh6.googleusercontent.com/-xZh_WQW6clw/TNAT0_n5z4I/AAAAAAAAENs/avrpwIwge3k/w558-h554-no/bw3.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="box">
+        <div class="col-lg-12" id="about">
+          <div class="col-lg-5 text-center header me">
+            <img class="img-circle me" height="140" width="140" src="https://scontent-a-iad.xx.fbcdn.net/hphotos-xpa1/t1.0-9/10441007_10202180539176445_7955040795142235919_n.jpg">
+          </div>
+            <div class="col-xs-7">
+            <p class="about lead">Hey everyone! Let's see a little about myself. I love photography, music, technology, saying something in very few words, and of course movies (saw too many last year!).</p>
+            <p>Currently studying Web Design and Development. Graduation date in September! Scared and excited am I!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+      <div class="col-lg-12 port" id="port">
+        <h2>Portfolio</h2>
+      </div><!--/span-->
+
+      <div class="clearfix"></div>
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/gcmd.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/gcmd/"><h2>Galaxy Command</h2></a>
+        <p>Task Management Web Application</p>
+        <p>Technologies used: jQuery, templating, HTML, CSS, Bootstrap</p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/decodeStation.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/JAndersonSMS/Site/"><h2>Decode Station</h2></a>
+        <p>Decode Station - translating code to be better understood. We deliver high quality video and written tutorials on web development.</p>
+        <p>Technologies used: jQuery, HTML, CSS, Red5, Foundation</p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/ftr.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/DFP/project-one_turnIn/index.html"><h2>Mythic Games</h2></a>
+        <p>Game: Face The Rainbow. Don't let the evil unicorns catch you. I bet buggy still, but good luck!</p>
+        <p>Technologies used: HTML, CSS, AS3, Flash, Bootstrap</p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/port.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/port/"><h2>Port (Old)</h2></a>
+        <p>Older Portfolio that has since been redesigned.</p>
+        <p>Technologies used: HTML, CSS, AS3, Bootstrap</p>
+      </div><!--/span-->
+      <div class="clearfix"></div>
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/vetHospital.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/wsp/project/index.html"><h2>Vet Hospital</h2></a>
+        <p>Redesign for an animal hospital in Charlotte NC.</p>
+        <p>Technologies used: HTML, CSS, Bootstrap</p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/randomSurvey.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/wdf/lab6/form.html"><h2>Random Survey</h2></a>
+        <p>Just a random survey asking about one's mode of transportation. And some more</p>
+        <p>Technologies used: HTML, CSS</p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/movieReviews.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/wdf/lab7/rss_link.html"><h2>RSS Links</h2></a>
+        <p>RSS Feed of four different movie reviews</p>
+        <p>Technologies used: HTML, CSS, RSS</p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/bridgeBet.png" alt="...">
+        </a>
+        <a href="http://jawa21.github.io/BridgeBet/"><h2>Bridge Bet</h2></a>
+        <p>Take the bet and build the bridge. AS3 group project. Role: Group Leader</p>
+        <p>Technologies used: HTML, CSS, Adobe Air, AS3, Flash, Bootstrap</p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="assets/images/lexicon.png" alt="...">
+        </a>
+        <a href="https://github.com/JAWA21/1401ASL_individ"><h2>Lexicon</h2></a>
+        <p>Faux photography store.</p>
+        <p>Technologies used: HTML, CSS, CSS, PHP, CodeIgniter, mySQL, </p>
+      </div><!--/span-->
+
+      <div class="col-6 col-sm-6 col-lg-4 port">
+        <a class="pull-left imgs" href="#">
+          <img class="media-object" src="http://placehold.it/360x100" alt="...">
+        </a>
+        <a href="https://github.com/JAWA21/markularity"><h2>Markularity</h2></a>
+        <p>Bookmarking web application. ASL group project. Role: Group Leader</p>
+        <p>Technologies used: HTML, CSS, cakePHP, mySQL, Bootstrap</p>
+      </div><!--/span-->
+
+      <div class="col-lg-12" id="resume">
+        <h2>Resume</h2>
+      </div><!--/span-->
+
+
+<?php
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['inputEmail']))  {
+
+  //Email information
+  $admin_email = "scatzz88@gmail.com";
+  $email = $_REQUEST['inputEmail'];
+  $subject = 'From your webpage';
+  $comment = $_REQUEST['inputComment'];
+
+  //send email
+  mail($admin_email, "$subject", $comment, "From:" . $email);
+
+  //Email response
+  echo '<div class="col-lg-12 port" id="contact">
+        <h2>Thank you for contacting me! I will get back to you.</h2>';
+  }
+
+  //if "email" variable is null, show the form
+  else  {
+?>
+
+<!-- Contact form -->
+      <div class="col-lg-12 port" id="contact">
+        <h2>Contact</h2>
+
+        <form class="form-horizontal" role="form" method="post" id="contactForm">
+          <div class="form-group">
+            <label for="inputName" class="col-sm-2 control-label">Your Name</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="inputName" placeholder="What's your mom call you?">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputEmail" class="col-sm-2 control-label">Your Email</label>
+            <div class="col-sm-4">
+              <input type="email" class="form-control" id="inputEmail" placeholder="How do people get in contact with you?">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="inputComment" class="col-sm-2 control-label">Message</label>
+            <div class="col-sm-4">
+              <textarea class="form-control" rows="3" id="inputComment" placeholder="What's up?"></textarea>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-4">
+              <button type="submit" class="btn btn-default pull-right">Send</button>
+            </div>
+          </div>
+        </form>
+      </div><!--/span-->
+
+<?php
+  }
+?>
+
+    </div><!-- /.container -->
+
+
+
+
+   <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+             <div class="address-bar">Twitter @scatzz88 | coderwall scatzz88 | 980-202-1303</div>
+            <p>&copy; Anderson 2014</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- child of the body tag -->
+    <span id="top-link-block" class="hidden">
+        <a href="#top" id="backToTop" class="well well-sm">
+            <i class="glyphicon glyphicon-chevron-up chevron"></i>
+        </a>
+    </span><!-- /top-link-block -->
+
+    <!-- Jquery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Bootstrap CDN javascript -->
+    <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <!-- Fonts: Didot, Source Sans -->
+    <script src="http://use.edgefonts.net/gfs-didot;source-sans-pro.js"></script>
+
+    <script type="text/javascript" src="main.js"></script>
+</body>
+</html>
